0e9a1ff1d8a75860d8f7de580ce33ee3a6529cd3
Igor Polyakov <iopguy@gmail.com>
2015-03-14 20:05:07 -0700

    first commit of the code

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+#![feature(core)]
+#![allow(non_camel_case_types)]
+use std::num::FromPrimitive;
+
+#[macro_export]
+macro_rules! strenum {
+    ($e:ident => $( $t:ident ),*  ) => {
+        #[derive(FromPrimitive, Debug)]
+        enum $e { $($t,)* }
+        impl Strenum for $e {
+            fn enumify(command: &str) -> Option<$e> {
+                match command {
+                    $( stringify!($t) => Some($e::$t), )*
+                    _ => None
+                }
+            }
+            
+            fn stringify() -> String {
+                let mut buf = Vec::<String>::new();
+                let mut n: u8 = 0;
+                
+                while let Some(val) = <Self>::from_u8(n) {
+                    buf.push(format!("{:?}", val));
+                    n += 1;
+                }
+                
+                buf.connect("\n")
+            }
+        }
+    }
+}
+
+trait Strenum {
+    fn enumify(command: &str) -> Option<Self>;
+    fn stringify() -> String;
+}
+
+
+strenum! {
+    testenum => a, b
+}
+
+#[test]
+fn stringify() {
+	assert_eq!(&*<testenum>::stringify(), "a\nb");
+}
+
+#[test]
+fn enumify() {
+	match <testenum>::enumify("b").unwrap() {
+		testenum::b => "success",
+		testenum::a => panic!("Got a instead of b"),
+	};
+}
+
